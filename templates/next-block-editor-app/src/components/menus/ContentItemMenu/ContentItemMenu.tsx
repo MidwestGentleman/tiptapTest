16f1fad8c8ce0d294de7597a6078a54f73c02cec
D <daniel@theuntappedpotential.com>
2024-11-10 19:50:52 -0500

    fixing vercel issue

--- templates/next-block-editor-app/src/components/menus/ContentItemMenu/ContentItemMenu.tsx
+++ templates/next-block-editor-app/src/components/menus/ContentItemMenu/ContentItemMenu.tsx
@@ -1,86 +1,61 @@
-import { Icon } from '@/components/ui/Icon'
+'use client'
+
+import { BubbleMenu as BaseBubbleMenu } from '@tiptap/react'
+import React, { useCallback, useRef, useEffect, useState } from 'react'
+import * as PopoverMenu from '@/components/ui/PopoverMenu'
+
 import { Toolbar } from '@/components/ui/Toolbar'
-import DragHandle from '@tiptap-pro/extension-drag-handle-react'
-import { Editor } from '@tiptap/react'
+import { Icon } from '@/components/ui/Icon'
+import { MenuProps } from '../types'
 
-import * as Popover from '@radix-ui/react-popover'
-import { Surface } from '@/components/ui/Surface'
-import { DropdownButton } from '@/components/ui/Dropdown'
-import useContentItemActions from './hooks/useContentItemActions'
-import { useData } from './hooks/useData'
-import { useEffect, useState } from 'react'
-
-export type ContentItemMenuProps = {
-  editor: Editor
-}
-
-export const ContentItemMenu = ({ editor }: ContentItemMenuProps) => {
-  const [menuOpen, setMenuOpen] = useState(false)
-  const data = useData()
-  const actions = useContentItemActions(editor, data.currentNode, data.currentNodePos)
+export const ContentItemMenu = React.memo(({ editor, appendTo }: MenuProps): JSX.Element | null => {
+  const [isMounted, setIsMounted] = useState(false)
+  const menuRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (menuOpen) {
-      editor.commands.setMeta('lockDragHandle', true)
-    } else {
-      editor.commands.setMeta('lockDragHandle', false)
-    }
-  }, [editor, menuOpen])
+    setIsMounted(true)
+    return () => setIsMounted(false)
+  }, [])
+
+  const shouldShow = useCallback(() => {
+    if (!isMounted) return false
+    return editor.isActive('paragraph') || editor.isActive('heading')
+  }, [editor, isMounted])
+
+  const onDelete = useCallback(() => {
+    editor.chain().focus().deleteSelection().run()
+  }, [editor])
+
+  if (!isMounted) return null
 
   return (
-    <DragHandle
-      pluginKey="ContentItemMenu"
+    <BaseBubbleMenu
       editor={editor}
-      onNodeChange={data.handleNodeChange}
+      pluginKey="contentItemMenu"
+      updateDelay={0}
       tippyOptions={{
-        offset: [-2, 16],
-        zIndex: 99,
+        appendTo: () => appendTo?.current || document.body,
+        placement: 'left',
+        offset: [0, 15],
+        popperOptions: {
+          modifiers: [{ name: 'flip', enabled: false }],
+        },
+        getReferenceClientRect: () => {
+          const selection = window.getSelection()
+          if (!selection?.rangeCount) return null
+          const range = selection.getRangeAt(0)
+          return range.getBoundingClientRect()
+        },
       }}
+      shouldShow={shouldShow}
     >
-      <div className="flex items-center gap-0.5">
-        <Toolbar.Button onClick={actions.handleAdd}>
-          <Icon name="Plus" />
-        </Toolbar.Button>
-        <Popover.Root open={menuOpen} onOpenChange={setMenuOpen}>
-          <Popover.Trigger asChild>
-            <Toolbar.Button>
-              <Icon name="GripVertical" />
-            </Toolbar.Button>
-          </Popover.Trigger>
-          <Popover.Content side="bottom" align="start" sideOffset={8}>
-            <Surface className="p-2 flex flex-col min-w-[16rem]">
-              <Popover.Close>
-                <DropdownButton onClick={actions.resetTextFormatting}>
-                  <Icon name="RemoveFormatting" />
-                  Clear formatting
-                </DropdownButton>
-              </Popover.Close>
-              <Popover.Close>
-                <DropdownButton onClick={actions.copyNodeToClipboard}>
-                  <Icon name="Clipboard" />
-                  Copy to clipboard
-                </DropdownButton>
-              </Popover.Close>
-              <Popover.Close>
-                <DropdownButton onClick={actions.duplicateNode}>
-                  <Icon name="Copy" />
-                  Duplicate
-                </DropdownButton>
-              </Popover.Close>
-              <Toolbar.Divider horizontal />
-              <Popover.Close>
-                <DropdownButton
-                  onClick={actions.deleteNode}
-                  className="text-red-500 bg-red-500 dark:text-red-500 hover:bg-red-500 dark:hover:text-red-500 dark:hover:bg-red-500 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-20"
-                >
-                  <Icon name="Trash2" />
-                  Delete
-                </DropdownButton>
-              </Popover.Close>
-            </Surface>
-          </Popover.Content>
-        </Popover.Root>
+      <div ref={menuRef}>
+        <Toolbar.Wrapper isVertical>
+          <PopoverMenu.Item icon="Trash" close={false} label="Delete" onClick={onDelete} />
+        </Toolbar.Wrapper>
       </div>
-    </DragHandle>
+    </BaseBubbleMenu>
   )
-}
+})
+
+ContentItemMenu.displayName = 'ContentItemMenu'
